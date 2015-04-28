ee1681052bbc409b7e57db5ab1bf4458a11604f1
Karl Floersch <karl.floersch@gmail.com>
2015-04-28 15:56:57 -0400

    Create page posting UI

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,16 +1,35 @@
-$( document ).ready(function() {
-    "use strict";
-    $(".comment-add").click(function() {
-        var $actionBox = $(this).parent().parent().parent();
+var commentClick = function() {
+        var $actionBox = $(this).parent().parent().parent().parent();
         var $commentBox = $actionBox.find(".comment-box");
         var comment = $commentBox.val();
+        var numLikes = 5;
         if (comment === ""){
             return;
         }
         $commentBox.val('');
         var hostname = $('<a>').prop('href', url).prop('hostname');
         var url = hostname + username;
-        $actionBox.find(".commentList").append('<li> <div class="commenterImage"> <img src="../../media/avatars/' + username + '.jpg"> </div> <div class="commentText"> <p class="">' + comment + '</p> <span class="date sub-text">' + fullname + ', on ' + today + '</span> </div> </li>');
+        $actionBox.find(".commentList").append('<li> <div class="commenterImage"> <img src="../../media/avatars/' + username + '.jpg"> </div> <div class="commentText"> <p class="">' + comment + '</p> <span class="date sub-text">' + numLikes + ' Likes - <a href="#">Like</a><br>' + fullname + ', on ' + today + '</span> </div> </li>');
+};
+
+$( document ).ready(function() {
+    "use strict";
+    $(".comment-add").click(commentClick);
+
+
+    $("#post-add").click(function() {
+        var $posts = $("#posts");
+        var post = $("#post-box").val();
+        if (post === ""){
+            return;
+        }
+        $("#post-box").val('');
+        var hostname = $('<a>').prop('href', url).prop('hostname');
+        var url = hostname + username;
+        $posts.prepend('<div class="detailBox"><div class="titleBox"><label>' + fullname + '</label></div><div class="commentBox"><p class="taskDescription">' + post + '</p></div><div class="actionBox"><ul class="commentList"></ul><form role="form"><div class="row"><div class="col-md-10"><textarea rows="1" cols="40" class="form-control comment-box" type="text" placeholder="Write a comment..."></textarea></div><div class="col-md-2"><button type="button" class="btn btn-default comment-add">Add</button></div></div></form></div></div>');
+        $(".comment-add").click(commentClick);
     });
 
 });
+
+
