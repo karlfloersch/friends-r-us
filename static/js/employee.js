96612b5de192ea86d6ee7161c445709943366d93
christian <maverickx5105@gmail.com>
2015-05-07 00:37:12 -0400

    email list done

--- static/js/employee.js
+++ static/js/employee.js
@@ -32,31 +32,32 @@ var create_advertisement = function() {
 
 };
 
-var get_all_advertisment = function(){
+var get_all_advertisment = function() {
+    $('#advertisement-list').toggle()
     var data = {
 
     };
     sendDefaultPOST('/produce-list-of-all-items-advertised/', data, function(response) {
         console.log(response.items);
         var i;
-        // A.adv_id, A.item_name, A.unit_price, A.num_aval_units
-        $('#advertisement-list').append('<tr><td>' + "advertisement ID"+ '</td><td>' + "item name" + '</td><td>' + "unit price" + '</td><td>'+ "num avaliable units"+'</td></tr>');
-        for(i = 0; i<response.items.length; i++){
-            $('#advertisement-list').append('<tr><td>' + response.items[i][0] + '</td><td>' + response.items[i][1] + '</td><td>' + response.items[i][2] + '</td><td>'+ response.items[i][3] + '</td><td class="delete-ad-list-row">Delete</td></tr>');
+        $('#advertisement-list').append('<tr><td>' + "advertisement ID" + '</td><td>' + "item name" + '</td><td>' + "unit price" + '</td><td>' + "num avaliable units" + '</td></tr>');
+        $('#advertisement-list').find("tr:gt(0)").remove();
+        for (i = 0; i < response.items.length; i++) {
+            $('#advertisement-list').append('<tr><td>' + response.items[i][0] + '</td><td>' + response.items[i][1] + '</td><td>' + response.items[i][2] + '</td><td>' + response.items[i][3] + '</td><td class="delete-ad-list-row">Delete</td></tr>');
         }
-        $('.delete-ad-list-row').click(function (){
+        $('.delete-ad-list-row').click(function() {
             var id = $(this).parent().children('td').eq(0).text();
             $(this).parent().remove();
-           
+
             var data = {
-                'id' : id 
+                'id': id
             };
             sendDefaultPOST('/delete-advertisement/', data, function(response) {
-            console.log(response);
+                console.log(response);
 
 
-        // in here, populate the shit
-        });
+                // in here, populate the shit
+            });
 
 
         });
@@ -76,23 +77,31 @@ var delete_advertisement = function() {
 
 };
 var generate_mailing_list = function() {
-
+    $('#mailing-list').toggle()
     var data = {
 
     };
+
     sendDefaultPOST('/generate-mailing-list/', data, function(response) {
-        console.log(response);
+        // console.log(response);
+        // mailing-list
+
+         var i;
+        $('#mailing-list').append('<tr><td>' + "Email" + '</td></tr>');
+        $('#mailing-list').find("tr:gt(0)").remove();
+        for (i = 0; i < response.items.length; i++) {
+            $('#mailing-list').append('<tr><td>' + response.items[i][0] + '</td></tr>');
+        }
+        
     });
 
 };
 
 
-$( document ).ready(function() {
+$(document).ready(function() {
     "use strict";
     $("#create-advertisement").click(create_advertisement);
     $("#delete-advertisement").click(create_advertisement);
     $("#produce-list-of-all-items-advertised").click(get_all_advertisment);
     $("#generate-mailing-list").click(generate_mailing_list);
 });
-
-
