51251049c9b2346869d345d618a9ca19001a1f4e
Joe Borormeo <Jrunna21@gmail.com>
2015-05-06 21:10:02 -0400

    Added queries. Updated schema file.

--- social_network/queries.py
+++ social_network/queries.py
@@ -187,27 +187,46 @@ def create_a_circle(owner_id, name, circle_type):
     #    circle_type +
     #    ")")
 
+#todo write method to get number of items available
+def get_number_available_units(adv_id):
+    cursor = connection.cursor()
+    cursor = execute('SELECT num_aval_units FROM advertisement WHERE adv_id=?',(adv_id))
+    num = cursor.fetchone()
+    return num[0]
 
-def purchase_one_or_advertised_item(ad_id, num_units, date, customer_acc_num):
+def validate_purchase_quantity(adv_id, num_units):
+    cursor = connection.cursor()
+    cursor = execute('SELECT num_aval_units FROM advertisement WHERE adv_id=?',(adv_id))
+    num = cursor.fetchone()
+    num_ = num[0]
 
-    cursor = connection.cursor(
-        "UPDATE advertisement SET num_aval_units = num_aval_units -1 WHERE adv_id= " +
-        ad_id)
-    cursor.execute()
+    if (num_ - num_units) > 0:
+        return True 
+    else:
+        return False
+
+
+def purchase_one_or_advertised_item(adv_id, num_units, date, customer_acc_num):
+    cursor = connection.cursor()
+    #cursor.execute(UPDATE advertisement SET num_aval_units = num_aval_units - num_units WHERE adv_id= " +
+    #   ad_id)
+  
+    cursor.execute('UPDATE adversitement SET num_aval_units= num_aval_units - 1 WHERE adv_id=?',(adv_id))
     date_now = time.strftime("%d/%m/%Y")
-    ts = date.isoformat()
-    cursor = connection.cursor("INSERT INTO buy (NULL," +
-                               num_units +
-                               "," +
-                               date_now +
-                               "," +
-                               ts +
-                               "," +
-                               customer_acc_num +
-                               "," +
-                               ad_id +
-                               ")")
-    cursor.execute()
+    time = datetime.datetime.now()
+    #cursor = connection.cursor("INSERT INTO buy (NULL," +
+    #                           num_units +
+    #                           "," +
+    #                           date_now +
+    #                           "," +
+    #                           ts +
+    #                           "," +
+    #                           customer_acc_num +
+    #                           "," +
+    #                           ad_id +
+    #                           ")")
+    cursor.execute('INSERT INTO buy(num_units, date, time, customer_acc_num, adv_id) VALUES(?,?,?,?,?)', (num_units, date_now, time, customer_acc_num, adv_id))
+
 
     # not complete
 
@@ -432,7 +451,6 @@ def remove_customer(cust_id, acc_id):
     cursor.execute('DELETE FROM customer WHERE cust_id=?',(cust_id))
     #Check to see if account is deleted when a customer is removed from the system.
 
-
 def update_customer(cust_id, rating, firstname_, lastname_, password_, gender_, address_, city_, state_, zipcode_, telephone_, email_, dob_):
     cursor = conneciton.cursor()
     cursor.execute('UPDATE customer SET email=?, rating=?, date_of_birth=? WHERE id =?',(email_, rating, dob_))
@@ -442,6 +460,18 @@ def update_customer_credit_card(account_id, credit_card_num):
     cursor = connection.cursor()
     cursor.execute('UPDATE account SET credit_card_num=? WHERE account_id=?',(credit_card_num, account_id))
 
+def customer_mailing_list():
+    cursor = connection.cursor()
+    cursor = execute('SELECT DISTINCT email FROM customer')
+    email_list = cursor.fetchall()
+    return email_list
+
+def item_suggestions(emp_id, cust_id):
+    #SELECT A.Item_Name, A.Advertisement_Id  FROM  Advertisement  A  WHERE A.Employee = ? AND A.Number_Of_Units>0  AND A.Type  IN (SELECT DISTINCT (A.Type) FROM Advertisement A INNER JOIN Purchase P INNER JOIN User U ON A.Advertisement_Id = P.Advertisement AND P.User = U.User_Id WHERE U.User_Id = ? ) 
+    cursor = connection.cursor()
+    cursor = execute('SELECT A.item_name, A.adv_id FROM advertisement A WHERE A.employee_id=? AND A.num_aval_units > 0 AND A.type IN (SELECT DISTINCT A.type FROM advertisement A INNER JOIN buy B INNER JOIN customer C ON A.adv_id = B.adv_id AND B.customer_acc_num = C.cust_id WHERE C.cust_id=?)',(emp_id, cust_id))
+    item_suggestions = cursor.fetchall()
+    return item_suggestions
 
 
 def add_employee(
@@ -614,3 +644,4 @@ def get_revenue_of_item(adv_id, type_in, customer_acc_num):
     cursor.execute('SELECT TS.adv_id, TS.item_name, TS.num_units * TS.unit_price  AS Revenue FROM TotalSale TS where TS.adv_id = ? OR TS.type = ? OR TS.customer_acc_num = ?',(adv_id, type_in, customer_acc_num))
     revenue_obj = cursor.fetchone()
     return revenue_obj
+
